d2b0e8e1d538c7653804ea68ac4f01c4f5467dc4
Vaibhav Goel <43861043+vaibhavgoel-github-1986@users.noreply.github.com>
2025-01-30 22:44:14 -0800

    Delete testing.py

--- testing.py
+++
@@ -1,9 +0,0 @@
-
-from Utilities.GetDependencies import get_dependencies
-import json
-import streamlit as st
-
-class_name = input("Enter the ABAP class name: ")  #zcl_cc_rate_plan
-
-dependencies = get_dependencies(class_name=class_name)
-print(json.dumps(dependencies, indent=4))  
